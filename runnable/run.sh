a4abf021eba3cd44db9109c091b4a7f9a04d787f
JJinIT <jinoh9272@naver.com>
2020-12-19 01:12:14 +0900

    Edit confs

--- runnable/run.sh
+++ runnable/run.sh
@@ -1,8 +1,5 @@
-cd /home/ubuntu/batch
-cd theone
-git pull
-cd ..
 docker-compose down
 docker-compose build producer subscriber
 docker-compose up --scale subscriber=3 -d
+./autoscaling.sh subscriber &
 
